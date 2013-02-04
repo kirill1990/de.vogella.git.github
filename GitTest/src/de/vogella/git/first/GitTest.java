cd2dbe13ad63b11c132eab4e86a5c05b63c1576a
kirill1990 <demyanov.kirill@gmail.com>
2013-02-04 16:45:27 +0400

    Initial commit

---
+++ GitTest/src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,16 @@
+package de.vogella.git.first;
+
+public class GitTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello gg");
+		System.out.println("test");
+	}
+
+}
